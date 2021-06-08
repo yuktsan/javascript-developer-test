311751d7b3ac1bdace377d881e15592d0c820e05
Caleb Chen <caleb.chen@peopleplan.com.au>
2021-06-08 22:18:04 +1000

    Implement a getArnieQuotes method to fetch quotes from a given urls

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,24 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return await Promise.all(
+      urls.map( async url => await handleRequest(url)))
+          .catch(error => console.error(`Error occurs in getArnieQuotes - ${error}`));
+};
+
+const handleRequest = async(url) => {
+    const respond = await httpGet(url);
+    const obj = JSON.parse(respond.body);
+
+    if(respond.status === 200) {
+        return {
+            "Arnie Quote": obj.message
+        };
+    }
+
+    return {
+        "FAILURE": obj.message
+    }
 };
 
 module.exports = {
